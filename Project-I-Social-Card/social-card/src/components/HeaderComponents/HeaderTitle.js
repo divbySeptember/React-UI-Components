ca8c734395573b240b1dc4bc324bbfa728d014b7
Anthony Johnson <lambda_school_loaner_82@Lambda-School-Loaner-82s-MacBook-Pro.local>
2019-03-18 19:24:37 -0400

    ....

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import './Header.css';
+import moment from 'moment'
 
 
-
+let date = moment().format('DD MMM');
 
 const HeaderTitle = () => {
     return (
         <div className="title">
             <h2> Lambda School </h2>
             <h3 className="userTag"> @LambdaSchool </h3>
+            <ul id="date">
+            <li>{date}</li>
+
+            </ul>
                 
             
         </div>
