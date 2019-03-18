774c2cef40b21bb0fcd8de1db98719a0760398cc
Anthony Johnson <lambda_school_loaner_82@Lambda-School-Loaner-82s-MacBook-Pro.local>
2019-03-18 19:11:39 -0400

    ...

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,17 @@
 import React from 'react';
 import './Header.css';
+import HeaderContent from "./HeaderContent";
+
+
+
+
+const HeaderContainer = () => {
+    return (
+        <div className="headerContainer">
+            
+            <HeaderContent />
+        </div>
+    )
+}
+
+export default HeaderContainer
---
+++ Project-I-Social-Card/social-card/src/components/Moment.js
@@ -0,0 +1,9 @@
+// import React  from 'react';
+// import Moment from 'moment';
+
+// const  Moment = () => {
+//     return ( 
+//         <Moment format="YYYY/MM/DD">{this.props.dateToFormat}</Moment>
+//     )
+// }
+// export default Moment
